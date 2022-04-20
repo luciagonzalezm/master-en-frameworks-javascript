39a2833f98d54cc0c09fc39af0acd05fc282b3c0
Lucía González <86147223+luciagonzalezm@users.noreply.github.com>
2022-04-20 16:26:34 +0200

    Create app.routing.ts

---
+++ aprendiendo_angular/src/app/app.routing.ts
@@ -0,0 +1,32 @@
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from "./components/home/home.component";
+import { BlogComponent } from "./components/blog/blog.component";
+import { FormularioComponent } from "./components/formulario/formulario.component";
+import { PeliculasComponent } from "./components/peliculas/peliculas.component";
+import { PaginaComponent } from "./components/pagina/pagina.component";
+import { ErrorComponent } from "./components/error/error.component";
+import { ArticleComponent } from "./components/article/article.component";
+import { SearchComponent } from "./components/search/search.component";
+import { ArticleNewComponent } from "./components/article-new/article-new.component";
+import { ArticleEditComponent } from "./components/article-edit/article-edit.component";
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'blog/articulo/:id', component: ArticleComponent},
+    {path: 'blog/crear', component: ArticleNewComponent},
+    {path: 'blog/editar/:id', component: ArticleEditComponent},
+    {path: 'formulario', component: FormularioComponent},
+    {path: 'peliculas', component: PeliculasComponent},
+    {path: 'pagina-de-pruebas', component: PaginaComponent},
+    {path: 'buscar/:search', component: SearchComponent},
+    {path: 'pagina-de-pruebas/:nombre/:apellidos', component: PaginaComponent},
+    {path: '**', component: ErrorComponent}
+];
+
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
