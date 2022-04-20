a72387f2531f238c47dd9375051d0d0985e6d5f2
Lucía González <86147223+luciagonzalezm@users.noreply.github.com>
2022-04-20 16:39:05 +0200

    Add files via upload

---
+++ aprendiendo_angular/src/app/components/blog/blog.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/models/article';
+import { ArticleService } from 'src/app/services/article.service';
+import { Global } from 'src/app/services/global';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css'],
+  providers: [ArticleService]
+})
+export class BlogComponent implements OnInit {
+  public title: string;
+  public articles!: Article[];
+  public url: string;
+
+  constructor(private _articleService: ArticleService) {
+    this.title = "Blog";
+    this.url = Global.url;
+  }
+
+  ngOnInit(): void {
+    this._articleService.getArticles().subscribe(
+      response => {
+        if (response.articles) {
+          this.articles = response.articles;
+        } else {
+
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+}
